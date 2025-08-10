e639de768aeed7392fab28a0776d22148954763e
Rachmat Wahyudi <rachmatsyalaffiliate@gmail.com>
2025-08-10 18:44:06 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HorrorElementsSpinner from './components/HorrorElementsSpinner'
+
+function App() {
+  return (
+    <div className="App">
+      <HorrorElementsSpinner />
+    </div>
+  )
+}
+
+export default App
